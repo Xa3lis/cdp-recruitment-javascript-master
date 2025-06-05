ef5c70e6776df7fba42ce0d1e33e9035f55cdbb3
Xa3lis <aerys59@gmail.com>
2025-06-05 17:56:17 +0200

    fix(): remove useless code

--- tests/index.spec.js
+++ tests/index.spec.js
@@ -1,4 +1,4 @@
-import { describe, it, expect, vi, beforeEach } from 'vitest'
+import { describe, it, expect, vi } from 'vitest'
 import { data } from '~mocks/data.js'
 import { exit, argv } from 'node:process'
 
